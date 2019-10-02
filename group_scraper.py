d8e30208464820137a4c9e242e9b8b026ac42701
voidful <voidful.stack@gmail.com>
2019-10-02 21:02:46 +0800

    add page support

--- group_scraper.py
+++ group_scraper.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.chrome.options import Options
 
@@ -9,7 +6,6 @@ import sys
 import time
 import argparse
 import csv
-import calendar
 import re
 
 parser = argparse.ArgumentParser(description='Non API public FB miner')
@@ -40,6 +36,7 @@ options.add_argument("--mute-audio")
 
 driver = webdriver.Chrome(executable_path="./chromedriver", options=options)
 
+
 class CollectPosts(object):
     """Collector of recent FaceBook posts.
            Note: We bypass the FaceBook-Graph-API by using a 
@@ -49,7 +46,7 @@ class CollectPosts(object):
            USE THIS FOR EDUCATIONAL PURPOSES ONLY. DO NOT ACTAULLY RUN IT.
     """
 
-    def __init__(self, ids=["oxfess"], corpus_file="posts.csv", depth=5, delay=2):
+    def __init__(self, ids=["oxfess"], corpus_file="posts.csv", depth=5, delay=5):
         self.ids = ids
         self.dump = corpus_file
         self.depth = depth + 1
@@ -60,7 +57,7 @@ class CollectPosts(object):
         # creating CSV header
         with open(self.dump, "w", newline='', encoding="utf-8") as save_file:
             writer = csv.writer(save_file)
-            writer.writerow(["Author","Author ID", "uTime", "Text"])
+            writer.writerow(["Author", "Author ID", "uTime", "Text"])
 
     def strip(self, string):
         """Helping function to remove all non alphanumeric characters"""
@@ -81,68 +78,6 @@ class CollectPosts(object):
         # Scroll down depth-times and wait delay seconds to load
         # between scrolls
         for scroll in range(self.depth):
-
-            # Scroll down to bottom
-            self.browser.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);")
-
-            # Wait to load page
-            time.sleep(self.delay)
-
-        # Once the full page is loaded, we can start scraping
-        with open(self.dump, "a+", newline='', encoding="utf-8") as save_file:
-            writer = csv.writer(save_file)
-            links = self.browser.find_elements_by_link_text("See more")
-            for link in links:
-                link.click()
-            posts = self.browser.find_elements_by_class_name(
-                "userContentWrapper")
-            poster_names = self.browser.find_elements_by_xpath(
-                "//a[@data-hovercard-referer]")
-
-            for count, post in enumerate(posts):
-                # Creating first CSV row entry with the poster name (eg. "Donald Trump")
-                analysis = [poster_names[count].text]
-
-                # Creating a time entry.
-                time_element = post.find_element_by_css_selector("abbr")
-                utime = time_element.get_attribute("data-utime")
-                analysis.append(utime)
-
-                # Creating post text entry
-                text = post.find_element_by_class_name("userContent").text
-                status = self.strip(text)
-                analysis.append(status)
-
-                # Write row to csv
-                writer.writerow(analysis)
-    def fatch_fbid(self,str):
-        have_fbid = re.search(re.compile(r"\&id=(\d+)"), str)
-        if have_fbid:
-            fbid = have_fbid.groups()[0]
-            return fbid
-
-        have_fbid = re.search(re.compile(r"\?id=(\d+)"), str)
-        if have_fbid:
-            fbid = have_fbid.groups()[0]
-            return fbid
-
-        have_fbid = re.search(re.compile(r"\/(\d+)\/"), str)
-        if have_fbid:
-            fbid = have_fbid.groups()[0]
-            return fbid
-
-        return None
-
-    def collect_groups(self, group):
-        # navigate to page
-        self.browser.get(
-            'https://www.facebook.com/groups/' + group + '/')
-
-        # Scroll down depth-times and wait delay seconds to load
-        # between scrolls
-        for scroll in range(self.depth):
-
             # Scroll down to bottom
             self.browser.execute_script(
                 "window.scrollTo(0, document.body.scrollHeight);")
@@ -179,6 +114,71 @@ class CollectPosts(object):
 
                 # Write row to csv
                 writer.writerow(analysis)
+                save_file.flush()
+
+    def fatch_fbid(self, str):
+        have_fbid = re.search(re.compile(r"\&id=(\d+)"), str)
+        if have_fbid:
+            fbid = have_fbid.groups()[0]
+            return fbid
+
+        have_fbid = re.search(re.compile(r"\?id=(\d+)"), str)
+        if have_fbid:
+            fbid = have_fbid.groups()[0]
+            return fbid
+
+        have_fbid = re.search(re.compile(r"\/(\d+)\/"), str)
+        if have_fbid:
+            fbid = have_fbid.groups()[0]
+            return fbid
+
+        return None
+
+    def collect_groups(self, group):
+        # navigate to page
+        self.browser.get(
+            'https://www.facebook.com/groups/' + group + '/')
+
+        # Scroll down depth-times and wait delay seconds to load
+        # between scrolls
+        for scroll in range(self.depth):
+            # Scroll down to bottom
+            self.browser.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            time.sleep(self.delay)
+
+        # Once the full page is loaded, we can start scraping
+        with open(self.dump, "a+", newline='', encoding="utf-8") as save_file:
+            writer = csv.writer(save_file)
+            links = self.browser.find_elements_by_link_text("See more")
+            for link in links:
+                link.click()
+            posts = self.browser.find_elements_by_class_name(
+                "userContentWrapper")
+            poster_names = self.browser.find_elements_by_xpath(
+                "//a[@data-hovercard-referer]")
+
+            for count, post in enumerate(posts):
+                # Creating first CSV row entry with the poster name (eg. "Donald Trump")
+                analysis = [poster_names[count].text]
+
+                analysis.append(self.fatch_fbid(poster_names[count].get_attribute("outerHTML")))
+
+                # Creating a time entry.
+                time_element = post.find_element_by_css_selector("abbr")
+                utime = time_element.get_attribute("data-utime")
+                analysis.append(utime)
+
+                # Creating post text entry
+                text = post.find_element_by_class_name("userContent").text
+                status = self.strip(text)
+                analysis.append(status)
+
+                # Write row to csv
+                writer.writerow(analysis)
+                save_file.flush()
 
     def collect(self, typ):
         if typ == "groups":
@@ -242,11 +242,14 @@ if __name__ == "__main__":
                 "Your email or password is missing. Kindly write them in credentials.txt")
             exit()
 
-    if args.groups:
-        C = CollectPosts(ids=args.groups, depth=args.depth)
-        C.login(email, password)
-        C.collect("groups")
-    elif args.pages:
-        C = CollectPosts(ids=args.pages, depth=args.depth)
-        C.login(email, password)
-        C.collect("pages")
+    try:
+        if args.groups:
+            C = CollectPosts(ids=args.groups, depth=args.depth)
+            C.login(email, password)
+            C.collect("groups")
+        elif args.pages:
+            C = CollectPosts(ids=args.pages, depth=args.depth)
+            C.login(email, password)
+            C.collect("pages")
+    finally:
+        print("close")
